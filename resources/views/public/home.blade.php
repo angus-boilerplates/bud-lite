61941439e3b05a254fce0cd6044e5b9de0b803e6
Angus <angus.goody@gmail.com>
2026-01-10 17:46:55 +0000

    livewire and flux

---
+++ resources/views/public/home.blade.php
@@ -0,0 +1,17 @@
+<x-layouts.public title="Bud | Boilerplate for Laravel Applications">
+    <x-page-container>
+        <h2 class="mb-2 text-lg font-medium text-heading">Features</h2>
+        <ul class="max-w-md space-y-1 text-body list-disc list-inside">
+            <li>
+                Admin panel
+            </li>
+            <li>
+                FluxUi components
+            </li>
+            <li>
+                Livewire 3
+            </li>
+        </ul>
+
+    </x-page-container>
+</x-layouts.public>
